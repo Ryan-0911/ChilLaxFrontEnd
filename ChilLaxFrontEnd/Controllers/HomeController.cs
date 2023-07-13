a351544c37905011c79df34ec94a09ed5dce00dc
Huang, Po-Hsiang <87964932+Ryan-0911@users.noreply.github.com>
2023-07-14 01:11:52 +0800

    1. 完成公告輪播 2. 完成專注時間設定 3. 完成畫面音效切換 4. 新增主頁圖文動畫 5. 置中會員圖示

--- ChilLaxFrontEnd/Controllers/HomeController.cs
+++ ChilLaxFrontEnd/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace ChilLaxFrontEnd.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        ChilLaxContext _context = new ChilLaxContext();
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -15,10 +16,10 @@ namespace ChilLaxFrontEnd.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var data = _context.Announcements.Where(a => a.StartDate <= DateTime.Now && a.EndDate >= DateTime.Now);
+            return View(data);
         }
 
-    
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
