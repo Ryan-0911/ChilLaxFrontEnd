85ba99af2bac4822a97db91d9bf5f8d78ca3f10b
kuoyenhunag <114103009+kuoyenhunag@users.noreply.github.com>
2023-07-18 16:41:25 +0800

    Revert "Merge branch 'checkout'"
    This reverts commit e0d760959faeb8b6eb457ffdee920353e5a677ee, reversing
    changes made to bdaf878d09bc85cad317eaa8d51a83ba62c00170.

--- ChilLaxFrontEnd/Models/Employee.cs
+++ ChilLaxFrontEnd/Models/Employee.cs
@@ -14,4 +14,6 @@ public partial class Employee
     public string EmpAccount { get; set; } = null!;
 
     public string EmpPassword { get; set; } = null!;
+
+    public bool? Available { get; set; }
 }
