80ae9e56db885eee64145f0e34568062b139060f
WanTing <wan.ting2669@gmail.com>
2023-07-18 16:15:22 +0800

    從code移過來

---
+++ ChilLaxFrontEnd/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChilLaxFrontEnd.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
--- ChilLaxFrontEnd/Program.cs
+++ ChilLaxFrontEnd/Program.cs
@@ -1,7 +1,15 @@
+using ChilLaxFrontEnd.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// 加入這段設定，就可以在Controllers的建構函式注入此物件
+builder.Services.AddDbContext<ChilLaxContext>(
+    option => option.UseSqlServer(builder.Configuration.GetConnectionString("ChilLaxConnection")
+    ));  // 建立一個 ChilLaxContext 物件出來
 
 var app = builder.Build();
 
