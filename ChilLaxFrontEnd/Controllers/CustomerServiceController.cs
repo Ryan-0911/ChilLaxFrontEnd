327e9ae52ef2024a91f5736a8f95fa0dd3ad0286
kuoyenhunag <114103009+kuoyenhunag@users.noreply.github.com>
2023-07-24 02:28:29 +0800

    測試客服系統
    測試客服系統

---
+++ ChilLaxFrontEnd/Controllers/CustomerServiceController.cs
@@ -0,0 +1,15 @@
+﻿using ChilLaxFrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChilLaxFrontEnd.Controllers
+{
+    public class CustomerServiceController : Controller
+    {
+        ChilLaxContext db = new ChilLaxContext();
+        public IActionResult Create()
+        {
+            var data = db.CustomerServices.ToList();
+            return View(data);
+        }
+    }
+}
