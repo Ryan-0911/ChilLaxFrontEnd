a4133b28ca89f34d72e24300bbe228c4e75f01f4
WanTing <wan.ting2669@gmail.com>
2023-07-24 11:30:58 +0800

    加入購物車失敗

--- ChilLaxFrontEnd/Controllers/ProductController.cs
+++ ChilLaxFrontEnd/Controllers/ProductController.cs
@@ -1,7 +1,14 @@
 ﻿using ChilLaxFrontEnd.Models;
+using ChilLaxFrontEnd.Models.DTO;
 using ChilLaxFrontEnd.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using System.Linq; // 加入這個命名空間以使用 LINQ 查詢
+using System.Collections.Generic; // 加入這個命名空間以使用 IEnumerable<T>
+using System.Threading.Tasks; // 加入這個命名空間以使用非同步 Task<T> 方法
+using System;
 
 namespace ChilLaxFrontEnd.Controllers
 {
@@ -27,6 +34,7 @@ namespace ChilLaxFrontEnd.Controllers
 
         public IActionResult List(CKeywordViewModel ckvm)
         {
+            // 關鍵字搜尋
             string keyword = ckvm.txtKeyword;
             IEnumerable<Product> datas = null;
             if (string.IsNullOrEmpty(keyword))
@@ -41,49 +49,80 @@ namespace ChilLaxFrontEnd.Controllers
 
             return View(datas);
 
-            //var datas = from p in db.Products
-            //            select p;
-            //return View(datas);
+
         }
 
-        public IActionResult Details()
+       
+
+        [HttpGet]
+        public async Task<ActionResult<ProductsPagingDTO>> GetProductsByCategory(string category, int page = 1)
         {
+            var productsInCategory = _context.Products.Where(p => p.ProductCategory == category).ToList();
+            //return Json(productsInCategory);
+
+            
+
+            int PageSize = 8;
+
+            // 計算商品總數量和總頁數
+            int totalProducts = productsInCategory.Count();
+            int totalPages = (int)Math.Ceiling((double)totalProducts / PageSize);
+
+            // 取得當前頁面的商品資料
+            var currentPageProducts = productsInCategory
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize)
+                .ToList();
+
+            // 回傳商品資料和分頁相關資訊
+            ProductsPagingDTO prodDTO = new ProductsPagingDTO();
+            prodDTO.TotalPages = totalPages;
+            prodDTO.ProductsResult = currentPageProducts;
+
+            return prodDTO;
+
+        }
+
+
+        public IActionResult AddToCart(int? id)
+        {
+            if(id == null)
+            {
+                return RedirectToAction("List");
+            }
+            ViewBag.product_id = id;
             return View();
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
+        [HttpPost]
+        public IActionResult AddToCart(CAddToCartViewModel cvm)
+        {
+            ChilLaxContext db = new ChilLaxContext();
+            Product prod = db.Products.FirstOrDefault(t => t.ProductId == cvm.txtFId);
+            if (prod != null)
+            {
+                string json = "";
+                List<CShoppingCartItem> cart = null;
+                if (HttpContext.Session.Keys.Contains(CDictionary.SK_PURCHASED_PRODUCTS_LIST))
+                {
+                    json = HttpContext.Session.GetString(CDictionary.SK_PURCHASED_PRODUCTS_LIST);
+                    cart = JsonSerializer.Deserialize<List<CShoppingCartItem>>(json);
+                }
+                else
+                    cart = new List<CShoppingCartItem>();
+                CShoppingCartItem item = new CShoppingCartItem();
+                item.price = (decimal)prod.ProductPrice;
+                item.productId = cvm.txtFId;
+                item.count = cvm.txtCount;
+                item.product = prod;
+                cart.Add(item);
+                json = JsonSerializer.Serialize(cart);
+                HttpContext.Session.SetString(CDictionary.SK_PURCHASED_PRODUCTS_LIST, json);
+            }
+            return RedirectToAction("List");
+        }
 
-        //    P_DetailViewModel pdvm = new P_DetailViewModel();
 
-        //    var product = await _context.Products.Include(p => p.ProductCategory).FirstOrDefaultAsync(m => m.ProductId == id);
-
-        //    if (product == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    else
-        //    {
-        //        pdvm.product = product;
-        //        if (product.ProductImg != null)
-        //        {
-        //            pdvm.ProductImg = ViewImage(product.ProductImg);
-        //        }
-        //    }
-        //    return View(pdvm);
-        //}
-
-        //private string ViewImage(byte[] arrayImage)
-        //{
-        //    // 二進位圖檔轉字串
-        //    string base64String = Convert.ToBase64String(arrayImage, 0, arrayImage.Length);
-        //    return "data:image/png;base64," + base64String;
-        //}
     }
 
 }
---
+++ ChilLaxFrontEnd/Models/DTO/ProductsPagingDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ChilLaxFrontEnd.Models.DTO
+{
+    public class ProductsPagingDTO
+    {
+        public int TotalPages { get; set; }
+        public List<Product> ProductsResult { get; set; }
+    }
+}
