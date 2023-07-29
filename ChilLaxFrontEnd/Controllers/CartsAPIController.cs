2ad9eeeed11716e08447df55a4581c801851336b
Meowen <124065983+tuv156123@users.noreply.github.com>
2023-07-29 20:06:53 +0800

    Update CartsAPIController.cs

--- ChilLaxFrontEnd/Controllers/CartsAPIController.cs
+++ ChilLaxFrontEnd/Controllers/CartsAPIController.cs
@@ -58,41 +58,40 @@ namespace ChilLaxFrontEnd.Controllers
 
         // 郁霖原本
         // GET: api/CartsAPI/Create/4
-        //    [HttpGet("Create/{id}")]
-        //    public async Task<string> Create(int id)
-        //    {
-        //        if (_context.Cart == null)
-        //            return "新增失敗";
+        [HttpGet("Create/{id}")]
+        public async Task<string> Create(int id)
+        {
+            if (_context.Cart == null)
+                return "新增失敗";
 
-        //        string json = HttpContext.Session.GetString(CDictionary.SK_LOINGED_USER);
-        //        Console.WriteLine(json);
-        //        Member member = JsonSerializer.Deserialize<Member>(json);
-        //        int Mid = member.MemberId;
-        //        int Cartqty = 1;
-        //        List<Cart> thisCart = _context.Cart.Where(c => c.MemberId == Mid).ToList();
+            string json = HttpContext.Session.GetString(CDictionary.SK_LOINGED_USER);
+            Console.WriteLine(json);
+            Member member = JsonSerializer.Deserialize<Member>(json);
+            int Mid = member.MemberId;
+            int Cartqty = 1;
+            List<Cart> thisCart = _context.Cart.Where(c => c.MemberId == Mid).ToList();
 
-        //        for (int i = 0; i < thisCart.Count; i++)
-        //        {
-        //            if (thisCart[i].ProductId == id) 
-        //            {
-        //                thisCart[i].CartProductQuantity += Cartqty;
-        //                _context.Cart.Update(thisCart[i]);
-        //                _context.SaveChanges();
-        //                return "已有此商品，數量更新成功";
-        //            }
-        //        }
+            for (int i = 0; i < thisCart.Count; i++)
+            {
+                if (thisCart[i].ProductId == id)
+                {
+                    thisCart[i].CartProductQuantity += Cartqty;
+                    _context.Cart.Update(thisCart[i]);
+                    _context.SaveChanges();
+                    return "已有此商品，數量更新成功";
+                }
+            }
 
-        //        Cart cart = new Cart();
-        //        cart.MemberId = Mid;
-        //        cart.ProductId = id;
-        //        cart.CartProductQuantity = Cartqty;
+            Cart cart = new Cart();
+            cart.MemberId = Mid;
+            cart.ProductId = id;
+            cart.CartProductQuantity = Cartqty;
 
-        //        _context.Cart.Add(cart);
-        //        await _context.SaveChangesAsync();
+            _context.Cart.Add(cart);
+            await _context.SaveChangesAsync();
 
-        //        return "新增成功";
-        //    }
-        //}
+            return "新增成功";
+        }
 
 
         // 琬亭修改
